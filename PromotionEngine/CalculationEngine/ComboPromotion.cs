97d1e88d9e74368e614586c957bafeeade5b42c8
narayanambk <narayanambk@gmail.com>
2020-09-01 17:08:46 +0530

    Implemented ApplyPromotion to combopromotion

--- PromotionEngine/CalculationEngine/ComboPromotion.cs
+++ PromotionEngine/CalculationEngine/ComboPromotion.cs
@@ -2,15 +2,47 @@
 using PromotionEngine.Models;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace PromotionEngine.CalculationEngine
 {
     public class ComboPromotion : IPromotion
     {
+        public IEnumerable<Item> Items { get; private set; }
+        public decimal FlatPrice { get; private set; }
+        public ComboPromotion(IEnumerable<Item> item, decimal flatAmount)
+        {
+            Items = item;
+            FlatPrice = flatAmount;
+        }
+
         public Cart ApplyPromotion(Cart cart)
         {
-            throw new NotImplementedException();
+            decimal total = default;
+
+            var items = cart.Items.Where(x => Items.Any(c => c.Product.SKU == x.Product.SKU)).ToList();
+
+            var numberOfCombos = Items.Select(x => items.Count(i => i.Product.SKU == x.Product.SKU)).Min();
+
+            if (numberOfCombos > 0)
+            {
+                total = FlatPrice * numberOfCombos;
+
+                var calculatedItems = new List<Item>();
+
+                foreach (var sku in Items.Select(x=>x.Product.SKU))
+                {
+                    calculatedItems.AddRange(items.Where(x => x.Product.SKU == sku).Take(numberOfCombos));
+                }
+
+                var uncalculatedItems = items.Except(calculatedItems);
+
+                total += uncalculatedItems.Any() ? uncalculatedItems.Sum(x => x.Product.Price) : default;
+
+                return new Cart(cart.Items.Except(items).ToList(), cart.Total + total);
+            }
+            return cart;
+
         }
     }
 }
