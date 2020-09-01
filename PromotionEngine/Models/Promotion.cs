14bc5b7c14c409c16773f4c7759fa3d2ab64e915
narayanambk <narayanambk@gmail.com>
2020-09-01 18:43:36 +0530

    Added a new model called Promotion

---
+++ PromotionEngine/Models/Promotion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Models
+{
+    public class Promotion
+    {
+        public char PromotedProductSku { get; private set; }
+        public int PromotionQuantity { get; private set; }
+        public decimal PromotionAmount { get; private set; }
+        public Promotion(char promotedProductSku, int promotionQuantity, decimal promotionAmount)
+        {
+            PromotedProductSku = promotedProductSku;
+            PromotionQuantity = promotionQuantity;
+            PromotionAmount = promotionAmount;
+        }
+    }
+}
