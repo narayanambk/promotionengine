ad2ff27e09676b358d158afe6b27f751d91b1429
narayanambk <narayanambk@gmail.com>
2020-09-01 13:02:56 +0530

    Implemented the ApplyPromotion in PromotionService.

--- PromotionEngine/CalculationEngine/PromotionService.cs
+++ PromotionEngine/CalculationEngine/PromotionService.cs
@@ -1,14 +1,40 @@
 ﻿using PromotionEngine.CalculationEngine.Services;
 using PromotionEngine.Models;
 using System;
+using System.Linq;
 
 namespace PromotionEngine.CalculationEngine
 {
     public class PromotionService : IPromotion
     {
+        public Item Item { get; private set; }
+        public PromotionService(Item item)
+        {
+            Item = item;
+        }
+
         public Cart ApplyPromotion(Cart cart)
         {
-            throw new NotImplementedException();
+            decimal total = default;
+            var items = cart.Items.Where(x => x.Product.SKU == Item.Product.SKU).ToList();
+
+            var totalQuantityToProcess = items.Sum(x => x.Quantity);
+
+            if (items != null && totalQuantityToProcess >= Item.Quantity)
+            {
+                var numberOfSets = (int)Math.Floor((decimal)totalQuantityToProcess / Item.Quantity);
+                total = numberOfSets * Item.Product.Price;
+
+                int processedProductCount = (numberOfSets * Item.Quantity);
+
+                var unprocessedQuantity = totalQuantityToProcess - processedProductCount;
+
+                total += unprocessedQuantity * items.FirstOrDefault().Product.Price;
+
+                return new Cart(items, cart.Total + total);
+            }
+
+            return cart;
         }
     }
 }
