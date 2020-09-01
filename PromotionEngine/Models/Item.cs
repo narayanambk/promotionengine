0ebf1658ba0242f0b9c6dbe5971339a01a16d944
narayanambk <narayanambk@gmail.com>
2020-09-01 12:19:42 +0530

    Added a model Item which holds the Product details and the quantity

---
+++ PromotionEngine/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Models
+{
+    public class Item
+    {
+        public Product Product { get; private set; }
+        public int Quantity { get; private set; }
+
+        public Item(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+    }
+}
