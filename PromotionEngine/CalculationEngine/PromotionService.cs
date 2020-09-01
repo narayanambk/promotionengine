656662d2fd36045ca9e2b35ff81bf354788cfbb2
narayanambk <narayanambk@gmail.com>
2020-09-01 12:42:49 +0530

    Added a new class called PromotionService which implements IPromotion

---
+++ PromotionEngine/CalculationEngine/PromotionService.cs
@@ -0,0 +1,14 @@
+﻿using PromotionEngine.CalculationEngine.Services;
+using PromotionEngine.Models;
+using System;
+
+namespace PromotionEngine.CalculationEngine
+{
+    public class PromotionService : IPromotion
+    {
+        public Cart ApplyPromotion(Cart cart)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
