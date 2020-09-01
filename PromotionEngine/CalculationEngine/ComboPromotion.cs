65ced8abee979f82a2f16b507aab6f98d15a8b99
narayanambk <narayanambk@gmail.com>
2020-09-01 12:51:54 +0530

    Added a new class to apply combo offer/promotion.

---
+++ PromotionEngine/CalculationEngine/ComboPromotion.cs
@@ -0,0 +1,16 @@
+﻿using PromotionEngine.CalculationEngine.Services;
+using PromotionEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.CalculationEngine
+{
+    public class ComboPromotion : IPromotion
+    {
+        public Cart ApplyPromotion(Cart cart)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
