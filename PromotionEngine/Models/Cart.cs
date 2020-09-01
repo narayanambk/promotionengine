9cbd0384c6b9a66789e56d0b778e8203994d03bb
narayanambk <narayanambk@gmail.com>
2020-09-01 12:36:16 +0530

    Added Cart model which holds the Items added to cart. SKU is the item.

---
+++ PromotionEngine/Models/Cart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Models
+{
+    public class Cart
+    {
+        private readonly List<Item> _items = new List<Item>();
+        public IReadOnlyList<Item> Items => _items.AsReadOnly();
+
+        public decimal Total { get; private set; }
+
+        public Cart(List<Item> items, decimal total)
+        {
+            _items = items;
+            Total = total;
+        }
+        public Cart(List<Item> items)
+        {
+            _items = items;
+            Total = default;
+        }
+    }
+}
