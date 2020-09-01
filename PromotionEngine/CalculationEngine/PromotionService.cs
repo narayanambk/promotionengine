2cdf9c86ad2e392c4c0d76ef776b14c30e5b8470
narayanambk <narayanambk@gmail.com>
2020-09-01 18:50:30 +0530

    Updated the service with new Promotion model.

--- PromotionEngine/CalculationEngine/PromotionService.cs
+++ PromotionEngine/CalculationEngine/PromotionService.cs
@@ -7,31 +7,29 @@ namespace PromotionEngine.CalculationEngine
 {
     public class PromotionService : IPromotion
     {
-        public Item Item { get; private set; }
-        public PromotionService(Item item)
-        {
-            Item = item;
-        }
+        public Promotion Promotion { get; private set; }
+
+        public PromotionService(Promotion promotion) => Promotion = promotion;
 
         public Cart ApplyPromotion(Cart cart)
         {
             decimal total = default;
-            var items = cart.Items.Where(x => x.Product.SKU == Item.Product.SKU).ToList();
+            var items = cart.Items.Where(x => x.Product.SKU == Promotion.PromotedProductSku).ToList();
 
             var totalQuantityToProcess = items.Sum(x => x.Quantity);
 
-            if (items != null && totalQuantityToProcess >= Item.Quantity)
+            if (items != null && totalQuantityToProcess >= Promotion.PromotionQuantity)
             {
-                var numberOfSets = (int)Math.Floor((decimal)totalQuantityToProcess / Item.Quantity);
-                total = numberOfSets * Item.Product.Price;
+                var numberOfSets = (int)Math.Floor((decimal)totalQuantityToProcess / Promotion.PromotionQuantity);
+                total = numberOfSets * Promotion.PromotionAmount;
 
-                int processedProductCount = (numberOfSets * Item.Quantity);
+                int processedProductCount = (numberOfSets * Promotion.PromotionQuantity);
 
                 var unprocessedQuantity = totalQuantityToProcess - processedProductCount;
 
                 total += unprocessedQuantity * items.FirstOrDefault().Product.Price;
 
-                return new Cart(items, cart.Total + total);
+                return new Cart(cart.Items.Except(items).ToList(), cart.Total + total);
             }
 
             return cart;
