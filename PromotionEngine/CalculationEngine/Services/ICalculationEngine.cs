468132db9b2736f44d03de938f2c4b64b8c19495
narayanambk <narayanambk@gmail.com>
2020-09-01 12:33:12 +0530

    Added IPromotion and ICalculationEngine, which are responsible for apply promotion and calculating final cart value.

---
+++ PromotionEngine/CalculationEngine/Services/ICalculationEngine.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.CalculationEngine.Services
+{
+    interface ICalculationEngine
+    {
+    }
+}
---
+++ PromotionEngine/CalculationEngine/Services/IPromotion.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.CalculationEngine.Services
+{
+    interface IPromotion
+    {
+    }
+}
