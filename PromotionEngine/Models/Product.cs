13d607c70c4cb5e9cab830478b286e3ebe31cd81
narayanambk <narayanambk@gmail.com>
2020-09-01 12:15:06 +0530

    Added a model called Product which holds the Name and Price of the product.

---
+++ PromotionEngine/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Models
+{
+    public class Product
+    {
+        public string SKU { get; private set; }
+        public decimal Price { get; private set; }
+
+        public Product(string sku, decimal price)
+        {
+            SKU = sku;
+            Price = price;
+        }
+    }
+}
